9431ab4d722b89fa7df7a1af98e24a2ff30ccda4
이정현 <juunghyun@ijeonghyeon-ui-MacBookAir.local>
2023-11-14 16:13:17 +0900

    주류 이름 검색 api

--- app.js
+++ app.js
@@ -324,7 +324,69 @@ app.get('/liquor/bookmark/:userID', async (req, res) => {
     }
 });
 
-//주류 찜하기 기능 api
+//주류 이름 검색 api
+app.get('/liquor/search/:liquorname', async (req, res) => {
+    try {
+        // 요청에서 liquorname 파라미터 가져오기
+        const liquorname = req.params.liquorname;
+
+        // 데이터베이스 연결 초기화
+        const conn = db.init();
+
+        // 데이터베이스 연결
+        db.connect(conn);
+
+        // liquor 테이블에서 해당 liquorname을 포함하는 주류들의 정보 가져오기
+        const query = `
+      SELECT 
+        liquorID,
+        liquorName,
+        liquorType,
+        liquorPrice,
+        liquorDetail
+      FROM liquor
+      WHERE liquorName LIKE ?
+    `;
+
+        // 쿼리 실행
+        const [rows] = await conn.promise().query(query, [`%${liquorname}%`]);
+
+        // 연결 종료
+        conn.end();
+
+        // 클라이언트에 응답 보내기
+        if (rows.length > 0) {
+            // 검색된 이름이 여러개라면 배열에 담아서 전송
+            if (rows.length > 1) {
+                const liquorsInfo = rows.map((row) => ({
+                    liquorID: row.liquorID,
+                    liquorName: row.liquorName,
+                    liquorType: row.liquorType,
+                    liquorPrice: row.liquorPrice,
+                    liquorDetail: row.liquorDetail
+                }));
+                res.json(liquorsInfo);
+            } else {
+                // 검색된 이름이 하나라면 객체로 전송
+                const liquorInfo = {
+                    liquorID: rows[0].liquorID,
+                    liquorName: rows[0].liquorName,
+                    liquorType: rows[0].liquorType,
+                    liquorPrice: rows[0].liquorPrice,
+                    liquorDetail: rows[0].liquorDetail
+                };
+                res.json(liquorInfo);
+            }
+        } else {
+            res.status(404).json({ error: '주류 정보를 찾을 수 없습니다.' });
+        }
+    } catch (error) {
+        console.error('에러:', error);
+        res.status(500).json({ error: '내부 서버 오류' });
+    }
+});
+
+    //주류 찜하기 기능 api
 app.post('/liquor/bookmark/:liquorID', async (req, res) => {
     try {
         // 요청에서 liquorID 파라미터 가져오기
